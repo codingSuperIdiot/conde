2df56107efb62fdb058cb4d6441c32ea34cedd24
wolf <shilei0228@sina.com>
2017-04-21 17:59:18 +0800

    updata

--- src/components/artList.vue
+++ src/components/artList.vue
@@ -5,12 +5,12 @@
         <li v-for="(tab,index) in tabList" @click="changeTab(index)" :class="{active : activeIndex == index}">{{tab.name}}</li>
       </ul>
       <ul class="content">
-        <router-link tag="li" v-for="article in artList" :to="{name: 'article',params:{id: article.id}}">
-          <div class="userInfo ">
+        <router-link tag="li" v-for="article in artList" :key="article.id" :to="{name: 'article',params:{id: article.id}}">
+          <div class="userInfo contLeft">
             <img :src="article.author.avatar_url" alt="">
             <span>{{article.author.loginname}}</span>
           </div>
-          <div class="title_wrapper">
+          <div class="title_wrapper contRight">
             <h3>{{article.title}}</h3>
             <span>发布于：{{article.last_reply_at | time}}</span>
             <span>浏览量：{{article.visit_count}}</span>
@@ -45,17 +45,23 @@
 
     methods:{
       getArtList:function(){
-        this.axios.get(basePath+"topics",this.req)
+      	console.log("start")
+        this.axios.get(basePath+"topics",{params : this.req})
           .then((res)=>{
-            if(res.data.success === true)
+      		console.log(res)
+            if(res.data.success)
               this.artList =res.data.data;
           })
           .catch((err)=>{
             console.log(err);
           })
       },
-      changeTab:function(){
-
+      changeTab:function(index){
+      	this.activeIndex = index;
+      	this.req.tab = this.tabList[index].type;
+      	console.log(this.req.tab);
+      	this.req.limit = 20;
+      	this.getArtList();
       }
     },
     mounted:function () {
@@ -64,22 +70,49 @@
   }
 </script>
 
-<style >
+<style>
   .tabList{
     background-color: #999;
-    padding: 0.1rem;
+    padding: 0.3rem 0.4rem;
+    display: flex;
   }
   .tabList li{
-    display: inline-block;
-    width: 1rem;
-    height: 0.6rem;
-    line-height: 0.6rem;
+    display: table-cell;
+    width: 1.8rem;
+    height: 1rem;
+    line-height: 1rem;
     color: #afa;
-    font-size: 0.35rem;
+    font-size: 0.55rem;
     border-radius: 4px;
+    text-align: center;
+    margin: 0 0.2rem;
   }
   .tabList li.active{
     background-color: #7fd07f;
     color: #fff;
+  }
+  .content li{
+    width: 100%;
+    font-size: 0.55rem;
+    padding: 0.5rem 5%;
+    border-bottom: 1px solid #ddd;
+  }
+  .contLeft{
+    display: inline-block;
+    width: 15%;
+  }
+  .contRight{
+    display: inline-block;
+    width: 75%;
+  }
+  .userInfo{
+    vertical-align: top;
+    text-align: center;
+    overflow: hidden;;
+  }
+  .userInfo img{
+    width: 1.71rem;
+    height: 1.71rem;
+    border-radius: 50%;
   }
 </style>
