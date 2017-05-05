3a5ecd1a3fa8232c0d68dd9ec6aea30f6ee289d6
wolf <shilei0228@sina.com>
2017-05-05 17:34:13 +0800

    list style
    Signed-off-by: wolf <shilei0228@sina.com>

--- src/components/artList.vue
+++ src/components/artList.vue
@@ -8,12 +8,15 @@
           <router-link tag="li" v-for="article in artList" :key="article.id" :to="{name: 'article',params:{id: article.id}}">
             <div class="userInfo contLeft">
               <img :src="article.author.avatar_url" alt="">
-              <span>{{article.author.loginname}}</span>
+              <!--<span>{{article.author.loginname}}</span>-->
             </div>
             <div class="title_wrapper contRight">
-              <h3><span :class="article.good | getTabClass(article.top)">{{article.tab | getArticleTab(article.good, article.top) }}</span>{{article.title}}</h3>
-              <span>发布于：{{article.last_reply_at | time}}</span>
-              <span>浏览量：{{article.visit_count}}</span>
+              <h3><span class="tab" :class="article.tab | getTabClass(article.good,article.top)">{{article.tab | getTab(article.good, article.top) }}</span>{{article.title}}</h3>
+              <p>
+                <span class="time">发布于：{{article.last_reply_at | time}}</span>
+                <span class="review"><img src="../assets/review.png" alt="">{{article.reply_count}}</span>
+                <span class="visit"><img src="../assets/visit.png">{{article.visit_count}}</span>
+              </p>
             </div>
           </router-link>
         </ul>
@@ -75,49 +78,88 @@
   }
 </script>
 
-<style>
+<style lang="scss" scoped="" type="text/css">
   .tabList{
     background-color: #999;
     padding: 0.3rem 0.4rem;
     display: flex;
+    li{
+      display: table-cell;
+      width: 1.8rem;
+      height: 1rem;
+      line-height: 1rem;
+      color: #afa;
+      font-size: 0.55rem;
+      border-radius: 4px;
+      text-align: center;
+      margin: 0 0.2rem;
+    }
   }
-  .tabList li{
-    display: table-cell;
-    width: 1.8rem;
-    height: 1rem;
-    line-height: 1rem;
-    color: #afa;
-    font-size: 0.55rem;
-    border-radius: 4px;
-    text-align: center;
-    margin: 0 0.2rem;
-  }
+
   .tabList li.active{
     background-color: #7fd07f;
     color: #fff;
   }
   .content li{
     width: 100%;
-    font-size: 0.55rem;
+    font-size: 0;
     padding: 0.5rem 5%;
     border-bottom: 1px solid #ddd;
   }
   .contLeft{
     display: inline-block;
     width: 15%;
+    margin-right: 5%;
   }
   .contRight{
     display: inline-block;
-    width: 75%;
+    width: 80%;
+    font-size: 0.55rem;
   }
   .userInfo{
     vertical-align: top;
     text-align: center;
-    overflow: hidden;;
+    overflow: hidden;
+    img{
+      width: 1.71rem;
+      height: 1.71rem;
+      border-radius: 50%;
+    }
   }
-  .userInfo img{
-    width: 1.71rem;
-    height: 1.71rem;
-    border-radius: 50%;
+  .tab{
+    padding: 0 8px;
+    margin-right: 0.2rem;
+    display: inline-block;
+    color: #fff;
+  }
+  .top{
+    background-color: #f70008;
+  }
+  .good{
+    background-color: #20d724;
+  }
+  .share{
+    background-color: deeppink;
+  }
+  .ask{
+    background-color: darkmagenta;
+  }
+  .job{
+    background-color: #101cff;
+  }
+  .default{
+    background-color: #aaa;
+  }
+  .time{
+    position: relative;
+    top: 0.2rem;
+  }
+  .visit,.review{
+    float: right;
+  }
+  .visit img,.review img{
+    width: 0.8rem;
+    position: relative;
+    top: 0.2rem;
   }
 </style>
